4fd79932b25bb1edc3cad8e5d09cb6663644d9a5
Oyeins-GUI <oyeinx7@gmail.com>
2022-10-23 21:36:07 +0000

    updated

--- tests/votr_test.ts
+++ tests/votr_test.ts
@@ -18,6 +18,9 @@ Clarinet.test({
 
         assertEquals(block.receipts.length, 1);
         assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectAscii("registration successful");
+
     },
 });
 
@@ -43,6 +46,9 @@ Clarinet.test({
 
         assertEquals(block.receipts.length, 2);
         assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectAscii("registration successful");
+        block.receipts[1].result.expectErr().expectUint(400);
     },
 });
 
@@ -53,24 +59,123 @@ Clarinet.test({
         let totalVoters = 10
         let contestant1 = accounts.get('wallet_1')!.address
         let contestant2 = accounts.get('wallet_2')!.address
-        let contestant3 = accounts.get('wallet_3')!.address
+
+        let block = chain.mineBlock([
+            Tx.contractCall(
+                "votr",
+                "register",
+                [types.ascii('stackerspool'), types.principal(organization)],
+                organization
+            ),
+            Tx.contractCall(
+                "votr",
+                "create-election",
+                [types.ascii('stackerspool'), types.ascii('clarity lead'), types.uint(totalVoters), types.list([types.principal(contestant1), types.principal(contestant2)])],
+                organization
+            )
+        ]);
+
+        assertEquals(block.receipts.length, 2);
+        assertEquals(block.receipts[1].events[0].type, "stx_transfer_event");
+        assertEquals(block.receipts[1].events[1].type, "nft_mint_event");
+        assertEquals(block.receipts[1].events[2].type, "nft_mint_event");
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectAscii("registration successful");
+        block.receipts[1].result.expectOk().expectUint(1);
+        
+    },
+});
+
+Clarinet.test({
+    name: "create-election: do not allow unregistered organizations to create an election",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        let organization = accounts.get('wallet_1')!.address
+        let totalVoters = 10
+        let contestant1 = accounts.get('wallet_1')!.address
+        let contestant2 = accounts.get('wallet_2')!.address
 
         let block = chain.mineBlock([
             Tx.contractCall(
                 "votr",
                 "create-election",
-                [types.ascii('stackerspool'), types.ascii('clarity lead'), types.uint(totalVoters), types.list([types.principal(contestant1), types.principal(contestant2), types.principal(contestant3)])],
+                [types.ascii('stackerspool'), types.ascii('clarity lead'), types.uint(totalVoters), types.list([types.principal(contestant1), types.principal(contestant2)])],
                 organization
             )
-            // Tx.contractCall(
-            //     "votr",
-            //     "create-election",
-            //     [types.ascii('stackerspool'), types.ascii('clarity lead'), types.principal(organization)],
-            //     organization
-            // )
         ]);
 
         assertEquals(block.receipts.length, 1);
         assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectErr().expectUint(402)
+    },
+});
+
+Clarinet.test({
+    name: "authorize-voters: allow only registered organizations that has created an election to authorize voters",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        let organization = accounts.get('wallet_1')!.address
+        let totalVoters = 10
+        let contestant1 = accounts.get('wallet_1')!.address
+        let contestant2 = accounts.get('wallet_2')!.address
+        let voter1 = accounts.get('wallet_3')!.address
+        let voter2 = accounts.get('wallet_4')!.address
+        let voter3 = accounts.get('wallet_5')!.address
+        let voter4 = accounts.get('wallet_6')!.address
+
+        let block = chain.mineBlock([
+            Tx.contractCall(
+                "votr",
+                "register",
+                [types.ascii('stackerspool'), types.principal(organization)],
+                organization
+            ),
+            Tx.contractCall(
+                "votr",
+                "create-election",
+                [types.ascii('stackerspool'), types.ascii('clarity lead'), types.uint(totalVoters), types.list([types.principal(contestant1), types.principal(contestant2)])],
+                organization
+            ),
+            Tx.contractCall(
+                "votr",
+                "authorize-voters",
+                [types.ascii('stackerspool'), types.uint(electionId), types.list([types.principal(voter1), types.principal(voter2), types.principal(voter3), types.principal(voter4)])], organization
+            )
+        ]);
+
+        assertEquals(block.receipts.length, 1);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectOk().expectAscii("registration successful");
+        block.receipts[1].result.expectErr().expectUint(401)
+        console.log(block.receipts);
+        
+    },
+});
+
+Clarinet.test({
+    name: "authorize-voters: don't allow registered organizations that has not created an election to authorize voters",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        let organization = accounts.get('wallet_1')!.address
+        let electionId = 1
+        let voter1 = accounts.get('wallet_3')!.address
+        let voter2 = accounts.get('wallet_4')!.address
+        let voter3 = accounts.get('wallet_5')!.address
+        let voter4 = accounts.get('wallet_6')!.address
+
+        let block = chain.mineBlock([
+            Tx.contractCall(
+                "votr",
+                "authorize-voters",
+                [types.ascii('stackerspool'), types.uint(electionId), types.list([types.principal(voter1), types.principal(voter2), types.principal(voter3), types.principal(voter4)])], organization
+            )
+        ]);
+
+        assertEquals(block.receipts.length, 1);
+        assertEquals(block.height, 2);
+
+        block.receipts[0].result.expectErr().expectUint(401)
+        console.log(block.receipts);
+        
     },
 });
